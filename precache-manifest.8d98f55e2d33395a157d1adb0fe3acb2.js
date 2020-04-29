9fa4d0f16d778f0f11bc02cd566c64a7afd0dbea
shir0206 <40990488+shir0206@users.noreply.github.com>
2020-04-29 11:28:39 +0300

    Updates

---
+++ precache-manifest.8d98f55e2d33395a157d1adb0fe3acb2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7a59a0edbea04ee64426601e9781131",
+    "url": "/ShirWeb/index.html"
+  },
+  {
+    "revision": "d6e3d547aa97595580ac",
+    "url": "/ShirWeb/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "85dbd3c10da60556d5c2",
+    "url": "/ShirWeb/static/js/2.bd42c3fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ShirWeb/static/js/2.bd42c3fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6e3d547aa97595580ac",
+    "url": "/ShirWeb/static/js/main.57ef503b.chunk.js"
+  },
+  {
+    "revision": "8172847a2cde78bdfcdd",
+    "url": "/ShirWeb/static/js/runtime-main.0757aa73.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ShirWeb/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/ShirWeb/precache-manifest.8d98f55e2d33395a157d1adb0fe3acb2.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ShirWeb/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});

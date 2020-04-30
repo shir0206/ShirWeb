f5fc70a329274ad8a2a94692fa6859c8cdff39e9
shir0206 <40990488+shir0206@users.noreply.github.com>
2020-04-30 13:51:38 +0300

    Add state to identify card button click

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { Intro } from "./Intro/Intro";
 import Demo from "./Demo";
 
 function App() {
+  const [buttonClicked, setButtonClicked] = useState(0);
   // <Intro/>
   //      <Demo/>
+
+  let cardContent;
+  if (buttonClicked === 0) {
+    cardContent = <Intro />;
+  } else if (buttonClicked === 1) {
+    cardContent = <p>hi im 2</p>;
+  } else if (buttonClicked === 2) {
+    cardContent = <p>hi im 3</p>;
+  }
 
   return (
     <div className="app">
       <div className="card">
-        
-        <div className="content-container"></div>
+        <div className="content-container">{cardContent}</div>
         <div className="button-container">
-          <div className="intro-container">
+          <div
+            className="intro-container"
+            onClick={() => {
+              console.log(0);
+              setButtonClicked(0);
+            }}
+          >
             <p>Intro</p>
           </div>
-          <div className="project-container">
+          <div
+            className="project-container"
+            onClick={() => {
+              console.log(1);
+
+              setButtonClicked(1);
+            }}
+          >
             <p>Projects</p>
           </div>
-          <div className="about-container">
+          <div
+            className="about-container"
+            onClick={() => {
+              console.log(2);
+
+              setButtonClicked(2);
+            }}
+          >
             <p>About</p>
           </div>
         </div>

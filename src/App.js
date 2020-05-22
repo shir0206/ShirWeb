b30c4a40e827c8bcfbfec8a357387f4940213204
shir0206 <40990488+shir0206@users.noreply.github.com>
2020-05-22 17:49:09 +0300

    Refractor Cloud

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from "react";
 
 import "./App.css";
-import "./cloud.css";
 
 import { Intro } from "./Intro/Intro";
 import { Project } from "./Project/Project";
 import { About } from "./About/About";
 import { Profile } from "./Profile/Profile";
 import { Portfolio } from "./Portfolio/Portfolio";
+import { Cloud } from "./Cloud/Cloud";
 
 function App() {
   const { height, width } = useWindowDimensions();
@@ -84,32 +84,6 @@ function App() {
 }
 
 export default App;
-
-function Cloud(props) {
-  let cloudClassName;
-  let cloudStyle;
-
-  if (props.openReadMore) {
-    cloudClassName = "change-shape cloud-read-more";
-    cloudStyle = { transform: "translate( 0%, 0%)" };
-  } else {
-    if (props.tab === 0) {
-      cloudClassName = "change-shape cloud-intro";
-      cloudStyle = { transform: "translate( -50%, -50%)" };
-    } else if (props.tab === 1) {
-      cloudClassName = "change-shape cloud-project";
-      cloudStyle = { transform: "translate( -25%, 5%)" };
-    } else if (props.tab === 2) {
-      cloudClassName = "change-shape cloud-about";
-      cloudStyle = { transform: "translate( -20%, 15%)" };
-    }
-  }
-  return (
-    <div className="cloud-container">
-      <div className={cloudClassName} style={cloudStyle} />
-    </div>
-  );
-}
 
 function getWindowDimensions() {
   const { innerWidth: width, innerHeight: height } = window;

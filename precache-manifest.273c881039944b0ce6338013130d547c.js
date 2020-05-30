817c78a66b81777e435fccf5d982f45eacd9d0f0
shir0206 <40990488+shir0206@users.noreply.github.com>
2020-05-30 15:58:04 +0300

    Updates

--- precache-manifest.273c881039944b0ce6338013130d547c.js
+++ precache-manifest.273c881039944b0ce6338013130d547c.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "501cbeb1f0ee1a9514fcd9101a47e847",
+    "revision": "ac97a4647154a6a33669ee134986b623",
     "url": "/ShirWeb/index.html"
   },
   {
-    "revision": "8a731338d437889fa7ab",
-    "url": "/ShirWeb/static/css/main.767b509f.chunk.css"
+    "revision": "3abccca05d631a1cf9e8",
+    "url": "/ShirWeb/static/css/main.ee8c461c.chunk.css"
   },
   {
     "revision": "86d69a8236539ed42c3c",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ShirWeb/static/js/2.fec9c747.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8a731338d437889fa7ab",
+    "revision": "3abccca05d631a1cf9e8",
     "url": "/ShirWeb/static/js/main.7392b3eb.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ShirWeb/precache-manifest.a1a981af4ecd29a2c32f56ca70244bef.js"
+  "/ShirWeb/precache-manifest.273c881039944b0ce6338013130d547c.js"
 );
 
 self.addEventListener('message', (event) => {

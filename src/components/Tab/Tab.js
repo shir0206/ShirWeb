7dc4e457490de906fb25e244ed4d57acb3620008
shir0206 <40990488+shir0206@users.noreply.github.com>
2020-05-30 11:05:05 +0300

    Recieve props from Tab

--- src/components/Tab/Tab.js
+++ src/components/Tab/Tab.js
@@ -2,35 +2,35 @@ import React, { useState } from "react";
 
 import "./tab.css";
 
-export const Tab = ({tab, setTab, }) => {
+export const Tab = (props) => {
   return (
     <div className="tab-container">
       <div
         className={
-          tab === 0 ? "intro-container tab-on" : "intro-container"
+          props.tab === 0 ? "intro-container tab-on" : "intro-container"
         }
         onClick={() => {
-          setTab(0);
+          props.setTab(0);
         }}
       >
         <p>Intro</p>
       </div>
       <div
         className={
-          tab === 1 ? "project-container tab-on" : "project-container"
+          props.tab === 1 ? "project-container tab-on" : "project-container"
         }
         onClick={() => {
-          setTab(1);
+          props.setTab(1);
         }}
       >
         <p>Projects</p>
       </div>
       <div
         className={
-          tab === 2 ? "about-container tab-on" : "about-container"
+          props.tab === 2 ? "about-container tab-on" : "about-container"
         }
         onClick={() => {
-          setTab(2);
+          props.setTab(2);
         }}
       >
         <p>About</p>

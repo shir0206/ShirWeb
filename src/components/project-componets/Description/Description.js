32dbb7e1dbad120c7ea309667507a3d1f49137eb
shir0206 <40990488+shir0206@users.noreply.github.com>
2021-01-30 23:05:00 +0200

    Change mobile style

--- src/components/project-componets/Description/Description.js
+++ src/components/project-componets/Description/Description.js
@@ -4,65 +4,21 @@ import "./description.css";
 
 export const Description = (props) => {
   const description = props.currentDescription.toString();
-  return <li className="description">{description}</li>;
 
-  let descriptionContent = "";
+  const generateReplacer = (regex, element) => (match) => {
+    const fixedWord = match.replaceAll(regex, "");
+    return `<${element}>${fixedWord}</${element}>`;
+  };
 
-  let stringFlag = true;
-  let stringContent = "";
-  let codeFlag = false;
-  let codeContent = "";
-  let boldFlag = false;
-  let boldContent = "";
-  let char = "";
-
-  for (let i = 0; i < description.length; i++) {
-    char = description[i];
-
-    if (char === "`") {
-      codeFlag = !codeFlag;
-
-      if (codeFlag) {
-        stringFlag = false;
-        descriptionContent += stringContent;
-        stringContent = "";
-      } else {
-        descriptionContent += <code>{codeContent}</code>;
-        stringFlag = true;
-        codeContent = "";
-      }
-    } else if (char === "*") {
-      boldFlag = !boldFlag;
-
-      if (boldFlag) {
-        stringFlag = false;
-        descriptionContent += stringContent;
-        stringContent = "";
-      } else {
-        descriptionContent += <b>{boldContent}</b>;
-        stringFlag = true;
-        boldContent = "";
-      }
-    } else {
-      if (codeFlag) {
-        codeContent += char;
-      }
-
-      if (boldFlag) {
-        boldContent += char;
-      }
-
-      if (stringFlag) {
-        stringContent += char;
-      }
-    }
-  }
-  stringFlag = false;
-  descriptionContent += stringContent;
-  stringContent = "";
-
-  return <li className="description">{descriptionContent}</li>;
+  return (
+    <li className="description">
+      <span
+        dangerouslySetInnerHTML={{
+          __html: description
+            .replace(/\*.+\*/, generateReplacer("*", "strong"))
+            .replace(/`.+`/, generateReplacer("`", "code")),
+        }}
+      />
+    </li>
+  );
 };
-
-// ``
-// **
---
+++ src/components/project-componets/Portfolio/slides.js
@@ -0,0 +1,131 @@
+export const slides = [
+  {
+    name: "Uninav",
+    description: [
+      "Developed *mobile web* application for a guided tour at Haifa University campus, written in `React.JS` (hooks).",
+      "The *map* is provided and manipulated via `OpenStreetMap` & `Leaflet` API's.",
+      "The points of interest & tracks are received from `Firebase`.",
+      "The *current location* of the user is obtained through the `Geolocation`.",
+      "When the app detects that the user is near a point of interest, it recommends that he read more detailed information about that point.",
+      "The URL's are manipulated via `React Router`.",
+    ],
+    gitLink: "https://github.com/shir0206/uninav",
+    webLink: "https://shir0206.github.io/uninav/",
+    playStoreLink: "",
+    tags: [
+      "React.js",
+      "JavaScript",
+      "HTML5",
+      "CSS3",
+      "Firebase",
+      "OpenStreetMaps",
+      "Geolocation",
+      "Leaflet",
+      "Router",
+      "GraphicDesign",
+      "Locale",
+    ],
+
+    image:
+      "https://res.cloudinary.com/doywfjryu/image/upload/v1612012470/shir-web/uninav_eqnatg.png",
+  },
+  {
+    name: "Radial Menu",
+    description: [
+      "Developed a *recursive & responsive* radial menu, written in `React.js`",
+      "The radial menu displays *the strcture* of the folders & images.",
+      "The data retreival is done by using `axios` and a server token.",
+      "When a *folder* is clicked, displayed the next level of it's folders and images.",
+      "When an *image* is clicked, it is displayed below, along with a gallery of all the rest of the images at the current level.",
+      "The position of the buttons on the screen is dynamically changing, using *trigonometric calculations*.",
+    ],
+    gitLink: "https://github.com/shir0206/radial-menu",
+    webLink: "https://shir0206.github.io/radial-menu/",
+    playStoreLink: "",
+    tags: [
+      "React.js",
+      "JavaScript",
+      "HTML5",
+      "CSS3",
+      "Axios",
+      "Recursion",
+      "trigonometry",
+      "Responsive",
+    ],
+
+    image:
+      "https://res.cloudinary.com/doywfjryu/image/upload/v1612014002/shir-web/radial_menu_myfxd6.png",
+  },
+  {
+    name: "Art Gallery",
+    description: [
+      "Developed a website in `React.js` for displaying my paintings & drawings.",
+      "The data is retrieved from `Firebase`.",
+      "*Search* images by keywords & tags.",
+      "This project is my *first* `React.js` web, the development took a month, while learning the most basic topics from the docs.",
+      "I tried to build the site while maintaining a *responsive* design in different sizes of screens.",
+      "The most challenging concept was to represent the image cards *responsively* so that the amount of columns would be updated according to the screen width.",
+    ],
+    gitLink: "https://github.com/shir0206/ArtGallery",
+    webLink: "https://shir0206.github.io/ArtGallery/",
+    playStoreLink: "",
+    tags: [
+      "React.js",
+      "JavaScript",
+      "HTML5",
+      "CSS3",
+      "Firebase",
+      "Search",
+      "ImageZoom",
+      "ScrollLock",
+    ],
+
+    image:
+      "https://res.cloudinary.com/doywfjryu/image/upload/v1591429275/shir-web/art-gallery_dnnzna.png",
+  },
+  {
+    name: "TropiX",
+    description: [
+      "TropiX is a *mobile app* game,  inspired by Tic-Tac-Toe, and designed for `Android` devices.",
+      "The code was written in `Java`.",
+      "The architecture of the app was designed using `MVC` model.",
+      "Permanent data is stored on the device using `SharedPreferance`.",
+      "The app supports different *languages* (Hebrew & English).",
+      "The screens, icons and colors were designed by mockups.",
+    ],
+    gitLink: "https://github.com/shir0206/TropiX-App",
+    webLink: "",
+    playStoreLink:
+      "https://play.google.com/store/apps/details?id=com.shirzabolotnyklein.tropix&hl=en",
+    tags: [
+      "Java",
+      "Android",
+      "MVC",
+      "MultiLanguage",
+      "RecyclerView",
+      "UX/UI",
+      "SharedPreference",
+      "TicTacToe",
+      "UnlockableCharacters",
+    ],
+    image:
+      "https://res.cloudinary.com/doywfjryu/image/upload/v1591429271/shir-web/tropix_fkexxi.png",
+  },
+  {
+    name: "Nyquist Optics",
+    description: [
+      "*Developed* an `Android` app for evaluating camera performances",
+      "The calculations are calculated by the user's input via Nyquist formulas",
+      "Translated user *requirements* into implementation.",
+      "The app was released in `Google Play` and there are *~50 active users* worldwide, as of 2021.",
+      "The app is the first `Android` app I developed and currently under refractor process, in order to meet additional requirements and additional calculations.",
+    ],
+    gitLink: "https://github.com/shir0206/NyquistOptics",
+    webLink: "",
+    playStoreLink:
+      "https://play.google.com/store/apps/details?id=com.shirzabolotnyklein.nyquistoptics&hl=en",
+    tags: ["Java", "Android", "PerformanceCalculator", "SharedPreferance"],
+    image:
+      "https://res.cloudinary.com/doywfjryu/image/upload/v1591429270/shir-web/nyquist-optics_kzeurl.png",
+  },
+];
